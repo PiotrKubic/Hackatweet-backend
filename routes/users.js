d9c6bffbaa3f4fc7f4421e12b5237469d02af247
Your name <b.kems@hotmail.fr>
2024-10-10 14:05:13 +0200

    routes signs

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,54 @@
 var express = require('express');
 var router = express.Router();
 
+const User = require('../models/users');
+const uid2 = require('uid2');
+const bcrypt = require('bcrypt')
 
 router.get('/', (req, res) => {
-  
+  User.find()
+  .then(data =>
+    res.json(data)
+  )
 });
+
+router.post('/signup', (req, res) => { 
+  User.findOne ({ username : {$regex: new RegExp(req.body.username, 'i')} })
+  .then (data => {
+    if (data === null) {
+      const newUser = new User ({
+
+        pseudo : req.body.pseudo,
+        username: req.body.username,
+        password: bcrypt.hashSync(req.body.password, 10),
+        token: uid2(32),
+        likedTweets: []
+
+      })
+      newUser.save()
+      .then(user => {
+        res.json({result : true, user})
+      })
+    } else {
+      res.json({result : false, error: 'Username already used betch'})
+    }
+  })
+})
+
+router.post('/signin', (req, res) => {
+ /* if (!checkBody(req.body, ['username', 'password'])) {
+    res.json({ result: false, error: 'Missing or empty fields' });
+    return;
+  } 
+*/
+  User.findOne({ username: req.body.username }).then(data => {
+    if (data && bcrypt.compareSync(req.body.password, data.password)) {
+      res.json({ result: true});
+    } else {
+      res.json({ result: false, error: 'User not found or wrong password' });
+    }
+  });
+});
+
 
 module.exports = router;
